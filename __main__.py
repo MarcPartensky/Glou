99ecc3cf954161a8387302678812421dcefdc1df
Marc Partensky <marc.partensky@gmail.com>
2020-09-03 00:50:19 +0200

    modification du fichier principal

--- __main__.py
+++ __main__.py
@@ -3,14 +3,18 @@ import os
 
 result = record()
 
-if result.startswith('quels sont mes micros'):
+if result.startswith('microphone'):
     print(sr.Microphone.list_microphone_names())
 
 elif result.startswith('website'):
     os.system('code /Users/marcpartensky/programs/website')
 
-elif result.startswith('fichiers'):
+elif result.startswith('dossier'):
     print(os.listdir())
+
+elif result.startswith('sauvegarde'):
+    message = result.replace('sauvegarde', '', 1).strip()
+    os.system(f'git add -A; git commit -m  "{message}"; git push')
 
 else:
     print(result)
